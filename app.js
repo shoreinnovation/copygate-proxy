7f16f5169d9948088ba0df8d5d30f5aca1ac3a44
lixgbg <henrik.sjostrand@gmail.com>
2020-03-15 11:48:21 +0100

    Minor fix

--- app.js
+++ app.js
@@ -23,9 +23,9 @@ for (route of routes) {
     app.use(route.route, proxy({ target: route.address, changeOrigin: true, logLevel: 'debug' } ));
 }
 */
-app.use('/api', proxy('http://localhost:8080/api', { logLevel: 'debug'} ));
-app.use('/api-docs', proxy('http://localhost:8080/api-docs', { logLevel: 'debug'} ));
-app.use('/', proxy('http://localhost:8100/', { logLevel: 'debug'} ));
+app.use('/api', proxy('http://localhost:3000/api', { logLevel: 'debug'} ));
+app.use('/api-docs', proxy('http://localhost:3000/api-docs', { logLevel: 'debug'} ));
+app.use('/', proxy('http://localhost:4200/', { logLevel: 'debug'} ));
 
 app.listen(9443, () => {
     console.log('Proxy listening on port 9443');
